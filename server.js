981b2c5ed0ad3854d8f416ecc9bb58892a07214f
yashwant1998 <yashwanthdesu@gmail.com>
2017-02-07 12:38:56 -1200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one.html',function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+    req.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
     });
 app.get('/article-two',function(req, res){
-      res.send('article two requested and will be served here'); 
+      req.send('article two requested and will be served here'); 
     });
 app.get('/article-three',function(req, res){
       res.send('article three requested and will be served here'); 
