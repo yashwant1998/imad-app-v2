9c4e692c015ae0a1b8d89cb0ed4d458d2b8cd9f8
yashwant1998 <yashwanthdesu@gmail.com>
2017-02-14 02:03:15 -1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ element.innerHTML='New value';
 var img = document.getElementById('madi');
 var marginLeft=0;
 function moveRight () { 
-    marginLeft = marginLeft+10;
+    marginLeft = marginLeft+1;
     img.style.marginLeft = marginLeft + 'px';
 }
     
