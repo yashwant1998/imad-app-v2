79599e2334d598b5c624bc0b110eebe98d6b6432
yashwant1998 <yashwanthdesu@gmail.com>
2017-02-14 02:01:08 -1200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,15 @@ console.log('Loaded!');
 var element =document.getElementById('main-text');
 element.innerHTML='New value';
 //move the elemnt
+
 var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight () { 
+    marginLeft = marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
+    
  img.onclick=function(){
-     img.style.marginLeft='100px';
+     var interval = setInterval(moveRight,100)
+     
  };
